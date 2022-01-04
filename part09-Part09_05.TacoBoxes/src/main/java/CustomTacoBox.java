d1e064d59dfe3e1768ceaa081f1e38adbc83406c
unknown <kavinmoreno@protonmail.com>
2022-01-04 09:16:24 -0600

    first interface implementation

---
+++ part09-Part09_05.TacoBoxes/src/main/java/CustomTacoBox.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kavin
+ */
+public class CustomTacoBox implements TacoBox {
+    
+    private int tacos;
+    
+    public CustomTacoBox(int tacos) {
+        this.tacos = tacos;
+    }
+    
+    @Override
+    public int tacosRemaining() {
+        return tacos;
+    }
+    
+    @Override
+    public void eat() {
+        if (tacos > 0) {
+            tacos -= 1;
+        }
+    }
+    
+}
