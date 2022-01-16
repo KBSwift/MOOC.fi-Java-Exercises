7a9c3099b256593a633fbd3abecec86c229dda18
unknown <kavinmoreno@protonmail.com>
2022-01-15 20:40:30 -0600

    Class diagram exercises

---
+++ part11-Part11_01.Customer/src/main/java/Customer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kavin
+ */
+public class Customer {
+    
+    private String name;
+    private String address;
+    private String email;
+    
+}
